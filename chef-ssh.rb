a4be95211829c9224082d643627821b756e310a2
Matt Robenolt <matt@ydekproductions.com>
2017-03-29 10:49:36 -0700

    chef-ssh 0.2.0

--- chef-ssh.rb
+++ chef-ssh.rb
@@ -1,9 +1,9 @@
 class ChefSsh < Formula
   desc "ssh using chef roles and nodes"
   homepage "https://github.com/mattrobenolt/chef-ssh"
-  url "https://github.com/mattrobenolt/chef-ssh/releases/download/0.1.0/chef-ssh-darwin-amd64", :using => :nounzip
-  version "0.1.0"
-  sha256 "1e1240f3ef4fd9a3429166e8069076ccceee21940e66af9b4da9ed9e78200cae"
+  url "https://github.com/mattrobenolt/chef-ssh/releases/download/0.2.0/chef-ssh-darwin-amd64", :using => :nounzip
+  version "0.2.0"
+  sha256 "a7a86f86e1a952af43bb9f001ec7307c81562f07bc311fc320ec0f40abfce51b"
 
   bottle :unneeded
 
