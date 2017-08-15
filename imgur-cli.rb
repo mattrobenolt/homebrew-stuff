df844cce58db1c5df2643d714e3760b0e3cabf4a
Matt Robenolt <matt@ydekproductions.com>
2017-08-15 10:46:13 -0700

    imgur-cli 0.0.2

--- imgur-cli.rb
+++ imgur-cli.rb
@@ -1,9 +1,9 @@
 class ImgurCli < Formula
   desc "upload clipboard image to imgur and get back a direct link"
   homepage "https://github.com/mattrobenolt/imgur-cli"
-  url "https://github.com/mattrobenolt/imgur-cli/releases/download/0.0.1/imgur", :using => :nounzip
-  version "0.0.1"
-  sha256 "f2eb101b4e522a0948e33eeb20ce7cd7e1fb7443c2a9480eb2d236fc783bb98c"
+  url "https://github.com/mattrobenolt/imgur-cli/releases/download/0.0.2/imgur", :using => :nounzip
+  version "0.0.2"
+  sha256 "157559c1771176fc4bd66285b06d7daa75d5b23a3060158a8c856c161fbb2843"
 
   bottle :unneeded
 
