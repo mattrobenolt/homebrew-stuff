5d022086b6c9ecdf074b22eb6f053403cb3ad1aa
Matt Robenolt <matt@ydekproductions.com>
2018-11-12 12:21:32 -0800

    email-me 0.3.1

--- email-me.rb
+++ email-me.rb
@@ -1,9 +1,9 @@
 class EmailMe < Formula
   desc "email me when a thing is done"
   homepage "https://github.com/mattrobenolt/email-me"
-  url "https://github.com/mattrobenolt/email-me/releases/download/0.3.0/email-me-darwin-amd64", :using => :nounzip
-  version "0.3.0"
-  sha256 "8f8860ec74df562830d9999a7065f34d14cc20870badcfcaae69a2205b483faf"
+  url "https://github.com/mattrobenolt/email-me/releases/download/0.3.1/email-me-darwin-amd64", :using => :nounzip
+  version "0.3.1"
+  sha256 "f836269a9ec7c90c45a1f26fc28a4c61d810b605a5c8898aafbb55e0a9155558"
 
   bottle :unneeded
 
